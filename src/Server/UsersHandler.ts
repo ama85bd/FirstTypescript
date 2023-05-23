43dad8df901e2ba57adc267751efccd23b94bc29
Asif M Ashique <asifm.ashique@petronas.com>
2023-05-23 15:08:36 +0600

    api doc

--- src/Server/UsersHandler.ts
+++ src/Server/UsersHandler.ts
@@ -25,10 +25,41 @@ export class UsersHandler extends BaseRequestHandler {
       case HTTP_METHODS.PUT:
         await this.handlePut();
         break;
+      case HTTP_METHODS.DELETE:
+        await this.handleDelete();
+        break;
 
       default:
         this.handleNotFound();
         break;
+    }
+  }
+  private async handleDelete() {
+    const operationAuthorized = await this.operationAuthorized(
+      AccessRight.DALETE
+    );
+    if (operationAuthorized) {
+      const parsedUrl = Utils.getUrlParameters(this.req.url);
+      if (parsedUrl) {
+        if (parsedUrl.query.id) {
+          const deleteResult = await this.userDBAccess.deleteUser(
+            parsedUrl.query.id as string
+          );
+          if (deleteResult) {
+            this.respondText(
+              HTTP_CODES.OK,
+              `user ${parsedUrl.query.id} deleted`
+            );
+          } else {
+            this.respondText(
+              HTTP_CODES.NOT_FOUND,
+              `user ${parsedUrl.query.id} not deleted!`
+            );
+          }
+        } else {
+          this.respondBadRequest('Missing user!');
+        }
+      }
     }
   }
 
@@ -57,13 +88,20 @@ export class UsersHandler extends BaseRequestHandler {
       const parsedUrl = Utils.getUrlParameters(this.req.url);
       if (parsedUrl) {
         const userId = parsedUrl.query.id;
-        if (userId) {
-          const user = await this.userDBAccess.getUserById(userId as string);
+        if (parsedUrl.query.id) {
+          const user = await this.userDBAccess.getUserById(
+            parsedUrl.query.id as string
+          );
           if (user) {
             this.respondJsonObject(HTTP_CODES.OK, user);
           } else {
             this.handleNotFound();
           }
+        } else if (parsedUrl.query.name) {
+          const users = await this.userDBAccess.getUserByName(
+            parsedUrl.query.name as string
+          );
+          this.respondJsonObject(HTTP_CODES.OK, users);
         } else {
           this.respondBadRequest('User not present!');
         }
--- src/User/UsersDBAccess.ts
+++ src/User/UsersDBAccess.ts
@@ -40,6 +40,44 @@ export class UsersDBAccess {
     });
   }
 
+  public async deleteUser(userId: string): Promise<boolean> {
+    const operationSuccess = await this.deleteUserFromDb(userId);
+    this.nedb.loadDatabase();
+    return operationSuccess;
+  }
+
+  private async deleteUserFromDb(userId: string): Promise<boolean> {
+    return new Promise((resolve, reject) => {
+      this.nedb.remove(
+        { id: userId },
+        (err: Error | null, numRemoved: number) => {
+          if (err) {
+            reject(err);
+          } else {
+            if (numRemoved === 0) {
+              resolve(false);
+            } else {
+              resolve(true);
+            }
+          }
+        }
+      );
+    });
+  }
+
+  public async getUserByName(name: string): Promise<IUser[]> {
+    const regEx = new RegExp(name);
+    return new Promise((resolve, reject) => {
+      this.nedb.find({ name: regEx }, (err: Error, docs: any[]) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(docs);
+        }
+      });
+    });
+  }
+
   private generateUserId() {
     return Math.random().toString(36).slice(2);
   }
